f2eeb0b70a4147305cfe8aeb10f6830b7263cab4
anu6376081311 <111556950+anu6376081311@users.noreply.github.com>
2023-02-17 18:00:03 +0530

    Add files via upload

---
+++ A23(3).cpp
@@ -0,0 +1,12 @@
+// Program to calculate the sum of two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum=0;
+    cout<<"Enter two numbers :";
+    cin>>a>>b;
+    sum = a+b;
+    cout<<"The sum of two numbers is:"<<sum;
+    return 0;
+}
