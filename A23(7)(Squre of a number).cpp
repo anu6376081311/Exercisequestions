149268bd68d3efe728dbed544048cdb3de2824ef
anu6376081311 <111556950+anu6376081311@users.noreply.github.com>
2023-02-17 18:32:42 +0530

    Add files via upload

---
+++ A23(7)(Squre of a number).cpp
@@ -0,0 +1,12 @@
+// Program to calculate the squre of a number
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,s;
+    cout<<"Enter a number :";
+    cin>>n;
+    s = n*n;
+    cout<<"The squre of the given number is :"<<s;
+    return 0;
+}
