630293bedc7af47c0ea498a5bc34bea6fa0264b5
anu6376081311 <111556950+anu6376081311@users.noreply.github.com>
2023-02-17 18:49:11 +0530

    Add files via upload

---
+++ A23(9)(Max in the two numbers).cpp
@@ -0,0 +1,14 @@
+// Program to find the maximum in the two numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a and b :";
+    cin>>a>>b;
+    if(a>=b)
+    cout<<"the maximum in the given two numbers is :"<<a;
+    else
+    cout<<"the maximum in the given two numbers is :"<<b;
+    return 0;
+}
