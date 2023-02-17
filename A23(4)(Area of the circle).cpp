807b52b1b15ac26f7c234acd70ece827ca62e6fb
anu6376081311 <111556950+anu6376081311@users.noreply.github.com>
2023-02-17 18:09:04 +0530

    Add files via upload

---
+++ A23(4)(Area of the circle).cpp
@@ -0,0 +1,12 @@
+// program to calculate the area of the circle
+#include<iostream>
+using namespace std;
+int main()
+{
+ float r,A;
+ cout<<"Enter the radius of the circle:";
+ cin>>r;
+ A = 3.14*r*r;
+ cout<<"The area of the circle is:"<<A;
+ return 0;
+}
